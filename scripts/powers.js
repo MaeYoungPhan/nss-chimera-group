6028a7c0b60e759663e3de7180a3ab2cdd3a5cfa
Pavankumar Patel <patelmedic2@gmail.com>
2022-11-03 11:16:37 -0500

    powers added

---
+++ scripts/powers.js
@@ -0,0 +1,28 @@
+import { getPowers, setPower } from "./database.js";
+
+const powers = getPowers();
+
+document.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.name === "power") {
+            setPower(parseInt(event.target.value))
+        }
+    }
+)
+
+export const chimeraPowers = () => {
+    let html = "<ul>"
+
+    const powerArray = powers.map(
+        (power) => {
+            return `<li>
+            <input type="radio" name="style" value="${power.id}" /> ${power.size}
+        </li>`
+        }
+    )
+
+    html += powerArray.join("")
+    html += "</ul>"
+    return html
+}
