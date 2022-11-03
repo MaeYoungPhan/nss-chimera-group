4b54a0b5833b21695529941b1d147e51934150b3
Grace Boaz <anna.g.boaz@gmail.com>
2022-11-03 11:12:58 -0500

    legs legs legs

---
+++ scripts/legs.js
@@ -0,0 +1,28 @@
+import { getLegs, setLegs } from "./database.js";
+
+const legs = getLegs();
+
+document.addEventListener(
+    "change", (event) => { 
+        if (event.target.name === "leg"){
+            setLegs(parseInt(event.target.value))
+        }
+    }
+)
+
+export const pickLegs = () => {
+    let html = "<ul>"
+
+    const listLegs = legs.map(
+        (leg) => {
+            return `
+            <li>
+                <input type="radio" name="leg" value="${leg.id}" /> ${leg.name}
+            </li>`
+        }
+    )
+    html += listLegs.join("")
+    html += "</ul>"
+
+    return html
+}
