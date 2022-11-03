e59dc3de72e25a4f139ee821807f3951bf4b3632
Grace Boaz <anna.g.boaz@gmail.com>
2022-11-03 10:48:02 -0500

    arms fresh out the oven

--- scripts/arms.js
+++ scripts/arms.js
@@ -1,11 +1,28 @@
-import { getArms } from "./database.js";
+import { getArms, setArms } from "./database.js";
 
 const arms = getArms();
 
 document.addEventListener(
     "change", (event) => { 
         if (event.target.name === "arm"){
-            
+            setArms(parseInt(event.target.value))
         }
     }
 )
+
+export const pickArms = () => {
+    let html = "<ul>"
+
+    const listArms = arms.map(
+        (arm) => {
+            return `
+            <li>
+                <input type="radio" name="arm" value="${arm.id}" /> ${arm.name}
+            </li>`
+        }
+    )
+    html += listArms.join("")
+    html += "</ul>"
+
+    return html
+}
