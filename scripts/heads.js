f90f26bf65945b1bbe838c9d59b0ee6ddf7a4380
nathanpyles.91@gmail.com <nathanpyles.91@gmail.com>
2022-11-03 11:57:15 -0400

    Head Games

---
+++ scripts/heads.js
@@ -0,0 +1,32 @@
+import { getHeads, setHeads } from "./database.js"
+
+
+const heads = getHeads()
+
+
+document.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.name === "head") {
+            setHeads(parseInt(event.target.value))
+        }
+    }
+)
+
+
+export const headStyles = () => {
+    let html = "<ul>"
+
+
+    const listHeads = heads.map(head => {
+        return `<li>
+            <input type="radio" name="head" value="${head.id}" /> ${head.name}
+        </li>`
+    }) 
+
+    html += listHeads.join("")
+    html += "</ul>"
+    
+    return html
+
+}
