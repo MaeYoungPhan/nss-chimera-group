449aff4694a48fe6f88c34e1b90cbbc15d2bb3f6
Grace Boaz <anna.g.boaz@gmail.com>
2022-11-03 10:24:20 -0500

    arm day

---
+++ scripts/arms.js
@@ -0,0 +1,11 @@
+import { getArms } from "./database.js";
+
+const arms = getArms();
+
+document.addEventListener(
+    "change", (event) => { 
+        if (event.target.name === "arm"){
+            
+        }
+    }
+)
